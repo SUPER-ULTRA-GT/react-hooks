49974ef5bdcf0ab8fbe25643f508b1adcba4a593
unknown <lucassantos2356@digitalcollege.internal>
2025-05-21 11:09:26 -0300

    versao1

--- src/pages/BoneEClaudio/index.jsx
+++ src/pages/BoneEClaudio/index.jsx
@@ -1,11 +1,8 @@
-const BoneEClaudio = () => {
-    return (  
-        <>
-        <div className="">
-
-        </div>
-        </>
-    );
-}
- 
-export default BoneEClaudio;
+[plugin:vite:import-analysis] Failed to resolve import "global.css" from "src/main.jsx". Does the file exist?
+C:/Users/lucassantos2356/Desktop/GT-09/carrinho/react-hooks/src/main.jsx:4:7
+3  |  import { createRoot } from 'react-dom/client';
+4  |  import App from './App.jsx';
+5  |  import 'global.css';
+   |          ^
+6  |  createRoot(document.getElementById('root')).render(/*#__PURE__*/ _jsxDEV(StrictMode, {
+7  |      children: /*#__PURE__*/ _jsxDEV(App, {}, void 0, false, {
